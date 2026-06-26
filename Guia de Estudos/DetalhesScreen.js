4baf8b697f00e8ea7c9823d16a5825e4abb0eb06
Mateusu.dev <mateusguimaraes71@gmail.com>
2026-06-25 21:21:41 -0300

    Update DetalhesScreen.js
    alterações de agenda

--- Guia de Estudos/DetalhesScreen.js
+++ Guia de Estudos/DetalhesScreen.js
@@ -1,14 +1,60 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { TextInput, Text } from "react-native-paper";
+import { DatePickerModal } from "react-native-paper-dates";
 
 export default function DetalhesScreen({ navigation }) {
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState(null);
+  const [dataFormatada, setDataFormatada] = useState("");
+
+  const onDismiss = () => {
+    setOpen(false);
+  };
+
+  const onConfirm = ({ date }) => {
+    setOpen(false);
+
+    if (date) {
+      setData(date);
+      setDataFormatada(date.toLocaleDateString("pt-BR"));
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Detalhes</Text>
+      <Text variant="headlineMedium" style={styles.titulo}>
+        Detalhes
+      </Text>
 
-      <Text style={styles.texto}>
+      <Text variant="bodyLarge" style={styles.texto}>
         Aqui aparecem mais informações sobre o item escolhido.
       </Text>
+
+      <TextInput
+        label="Data"
+        mode="outlined"
+        textColor='black'
+        value={dataFormatada}
+        editable={false}
+        style={styles.input}
+        onPressIn={() => setOpen(true)}
+        right={
+          <TextInput.Icon
+            icon="calendar"
+            onPress={() => setOpen(true)}
+          />
+        }
+      />
+
+      <DatePickerModal
+        locale="pt-BR"
+        mode="single"
+        visible={open}
+        onDismiss={onDismiss}
+        date={data}
+        onConfirm={onConfirm}
+      />
 
       <Button
         title="Voltar para Home"
@@ -25,13 +71,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   titulo: {
-    fontSize: 24,
     textAlign: "center",
     marginBottom: 20,
+    color: "black",
   },
   texto: {
-    fontSize: 16,
     textAlign: "center",
+    marginBottom: 20,
+    color: "black",
+  },
+  input: {
+    width: 320,
+    height: 55,
+    backgroundColor: 'white',
     marginBottom: 20,
   },
 });
